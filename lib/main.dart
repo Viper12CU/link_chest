6b9ac94d2a03865fff52127b28e6c582d30b4e76
Viper12CU <fabianalejandrolemus2@gmail.com>
2026-07-20 10:59:14 -0400

    feat: Update system UI overlay style and remove unused shared handling code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:link_chest/database/database.dart';
 import 'package:link_chest/providers/category_provider.dart';
 import 'package:link_chest/providers/category_selected_provider.dart';
@@ -13,6 +14,13 @@ import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 final navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Color(0xFFF0F2F8), 
+      statusBarIconBrightness: Brightness.dark, 
+      systemNavigationBarColor: Color(0xFFF0F2F8),
+    ),
+  );
   WidgetsFlutterBinding.ensureInitialized();
   await DatabaseHelper().init();
   runApp(
@@ -79,65 +87,6 @@ class _AppState extends State<App> {
         });
   }
 
-  // void _handleShared(List<SharedMediaFile> files) async {
-  //   final CategoryProvider categoryProvider = Provider.of<CategoryProvider>(
-  //     context,
-  //     listen: false,
-  //   );
-
-  //   if (categoryProvider.categories.isEmpty) {
-  //     await categoryProvider.loadAll();
-  //   }
-
-  //   _logSharedFlow('Procesando ${files.length} archivo(s) compartido(s)');
-
-  //   if (files.isEmpty) return;
-
-  //   final text = files
-  //       .where((f) => f.type == SharedMediaType.text)
-  //       .map((f) => f.path)
-  //       .join();
-
-  //   _logSharedFlow(
-  //     'Contenido de texto detectado: ${text.isEmpty ? "vacío" : text}',
-  //   );
-
-  //   if (text.isEmpty) return;
-
-  //   final urlMatch = RegExp(r'https?://\S+').firstMatch(text);
-  //   final url = urlMatch?.group(0) ?? text;
-
-  //   _logSharedFlow('URL resuelta: $url');
-
-  //   final ctx = navigatorKey.currentContext;
-  //   if (ctx == null) {
-  //     _logSharedFlow(
-  //       'navigatorKey.currentContext es null; no se puede abrir AddLinkSheet',
-  //     );
-  //     return;
-  //   }
-
-  //   final defaultCategory = categoryProvider.categories.firstWhere(
-  //     (c) => c.id == DatabaseHelper.defaultCategoryId,
-  //     orElse: () => categoryProvider.categories.first,
-  //   );
-
-  //   final linkToAdd = LinkModel(
-  //     id: null,
-  //     title: 'Hola',
-  //     url: url,
-  //     categoryId: defaultCategory.id!,
-  //   );
-
-  //   _logSharedFlow(
-  //     'Abriendo AddLinkSheet para la categoría ${defaultCategory.id}',
-  //   );
-
-
-  //   if (context.mounted) {
-  //     AddLinkSheet.show(ctx, defaultCategory, linkToEdit: linkToAdd, isEditing: true, isShared: true);
-  //   }
-  // }
 
   @override
   void dispose() {
